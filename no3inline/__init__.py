7896191e96bbf0b43a6d0bff2646ebd586a972c4
Adam Vajda <vjdad4m@gmail.com>
2023-08-20 18:43:02 +0200

    init fix

--- no3inline/__init__.py
+++ no3inline/__init__.py
@@ -4,7 +4,7 @@ def calculate_reward(m):
     return no3inline.wrapper.calculate_reward(m)
 
 try:
-    from no3inline.collinear_cuda import calculate_reward_cuda
+    from no3inline._collinear_cuda import calculate_reward_cuda
 except ImportError:
     def calculate_reward_cuda(*args, **kwargs):
         raise ImportError("The CUDA module was not compiled. Please recompile with CUDA support.")
