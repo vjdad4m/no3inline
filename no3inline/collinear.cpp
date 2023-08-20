d7ca19b3c80ac15097b20a0f5b55b05f49f711a0
Adam Vajda <vjdad4m@gmail.com>
2023-08-20 18:32:11 +0200

    Updated kernel

--- no3inline/collinear.cpp
+++ no3inline/collinear.cpp
@@ -1,7 +1,10 @@
 #include <torch/extension.h>
+torch::Tensor reward_cuda(torch::Tensor m);
 
-torch::Tensor countCollinear(torch::Tensor points);
+torch::Tensor calculate_reward_cuda(torch::Tensor m) {
+    return reward_cuda(m);
+}
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-    m.def("count_collinear", &countCollinear, "Count Collinear Points");
+    m.def("calculate_reward_cuda", &reward_cuda, "Reward (CUDA)");
 }
--- no3inline/collinear_kernel.cu
+++ no3inline/collinear_kernel.cu
@@ -1,11 +1,11 @@
-#include <torch/extension.h>
+#include <torch/types.h>
 
 typedef long long ll;
 
-typedef struct {
+struct pt {
     ll x;
     ll y;
-} pt;
+};
 
 __device__ int turn(pt a, pt b, pt c) {
     if ((b.x - a.x) * (c.y - a.y) - (c.x - a.x) * (b.y - a.y) > 0) return 1;
@@ -13,28 +13,39 @@ __device__ int turn(pt a, pt b, pt c) {
     else return 0;
 }
 
-__global__ void countCollinearKernel(pt *p, int p_size, int *result) {
-    const int i = blockIdx.x;
-    const int j = blockIdx.y * blockDim.y + threadIdx.y;
-
-    if (i < p_size && j < p_size && j > i) {
-        for (int l = j + 1; l < p_size; l++) {
-            if (!turn(p[i], p[j], p[l])) {
-                atomicAdd(result, 1);
+__global__ void rewardKernel(const pt* points, const int p_size, int* result) {
+    const int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < p_size) {
+        for (int j = i + 1; j < p_size; j++) {
+            for (int l = j + 1; l < p_size; l++) {
+                if (!turn(points[i], points[j], points[l])) {
+                    atomicAdd(result, 1);
+                }
             }
         }
     }
 }
 
-torch::Tensor countCollinear(torch::Tensor points) {
-    const int p_size = points.size(0);
-    auto result = torch::zeros({1}, torch::kInt32).to(points.device());
+torch::Tensor reward_cuda(torch::Tensor m) {
+    auto m_size = m.size(0);
+    auto points = torch::empty({m_size * m_size, 2}, torch::kInt64).to(m.device());
+    auto count = torch::zeros({1}, torch::kInt32).to(m.device());
 
-    dim3 block_dim(1, 256);  // Here, 1 block in x and 256 threads in y.
-    dim3 grid_dim(p_size, (p_size + block_dim.y - 1) / block_dim.y);
-    
-    countCollinearKernel<<<grid_dim, block_dim>>>(reinterpret_cast<pt*>(points.data_ptr()), p_size, result.data_ptr<int>());
+    int index = 0;
+    for (int i = 0; i < m_size; i++) {
+        for (int j = 0; j < m_size; j++) {
+            if (m[i][j].item<int>()) {
+                points[index][0] = i;
+                points[index][1] = j;
+                index++;
+            }
+        }
+    }
 
-    cudaDeviceSynchronize();
-    return result;
+    const int threads = 256;
+    const int blocks = (index + threads - 1) / threads;
+
+    rewardKernel<<<blocks, threads>>>(reinterpret_cast<pt*>(points.data_ptr()), index, count.data_ptr<int>());
+
+    return count;
 }
