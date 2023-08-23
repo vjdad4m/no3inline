704373c66a56738529be3689f0f96ca1f51de373
Adam Vajda <vjdad4m@gmail.com>
2023-08-23 19:53:58 +0200

    Parameterized model

--- no3inline/config.py
+++ no3inline/config.py
@@ -1,15 +1,16 @@
+#################################################################
+# interesting values of N
+# 1. 10-16 : can we find solution? we can verify as all are known
+# 2. 17-18 : unpublished solutions may exist
+# 3. 19-46 : not all solutions are known
+# 4. 48-50-52 : a single solution is known
+# 5. 47-49-51-52< : no solution is known
+#################################################################    
+
 HYPERPARAMETERS = {
     'RUN_NAME': 'test_N=6',
     'N': 6,
-        #################################################################
-        # interesting values of N
-        # 1. 10-16 : can we find solution? we can verify as all are known
-        # 2. 17-18 : unpublished solutions may exist
-        # 3. 19-46 : not all solutions are known
-        # 4. 48-50-52 : a single solution is known
-        # 5. 47-49-51-52< : no solution is known
-        #################################################################    
-
+    'MODEL': 'convnet', # 'convnet' or 'resnet'
     'LEARNING_RATE': 0.001,
     'N_ROLLOUTS': 100,
     'N_EPOCHS': 200,
